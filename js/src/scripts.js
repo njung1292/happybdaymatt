ed1929f40bf4ccf845d33047fce64f67a7412d76
Jing Xiao <notajingoist@gmail.com>
2014-02-02 15:44:39 -0500

    draggable pokepuffs and pop up froakiegit add .

--- js/src/scripts.js
+++ js/src/scripts.js
@@ -2,16 +2,29 @@ var SITE = {
 	init: function() {
 		this.$document = $(window.document);
 		this.$body = $('body');
+	
 		this.cacheVars();
 		this.bindEvents();
+
+		var context = this;
+		setTimeout(function() {
+			context.$froakie.addClass('appear');
+		});
+
+		this.initDraggable();
 	},
 
 	cacheVars: function() {
-
+		this.$froakie = $('#froakie');
+		this.$pokepuffs = $('.pokepuff');
 	},
 
 	bindEvents: function() {
 		this.$body.on('click', this.functionName.bind(this));
+	},
+
+	initDraggable: function() {
+		this.$pokepuffs.draggable();
 	},
 
 	functionName: function(e) {
